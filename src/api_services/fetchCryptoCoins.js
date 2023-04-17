c68f92ffe092a5d9ba7ee6ddf694d84a6ccc1b1b
shubham <shubham.bhoyar077@gmail.com>
2023-04-17 17:53:36 +0530

    Update as with try and catch

--- src/api_services/fetchCryptoCoins.js
+++ src/api_services/fetchCryptoCoins.js
@@ -1,24 +1,28 @@
 const fectCrtyptoCoins = async () => {
-  const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?${new URLSearchParams({
-    vs_currency: 'usd',
-    order: 'market_cap_desc',
-    per_page: 20,
-    page: 1,
-    sparkline: false,
-    locale: 'en',
-  })}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  const data = await response.json();
-  const cryptoCoins = (data.map((coin) => ({
-    id: coin.id,
-    name: coin.name,
-    image: coin.image,
-    current_price: coin.current_price,
-  })));
-  return cryptoCoins;
+  try {
+    const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?${new URLSearchParams({
+      vs_currency: 'usd',
+      order: 'market_cap_desc',
+      per_page: 20,
+      page: 1,
+      sparkline: false,
+      locale: 'en',
+    })}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    const cryptoCoins = (data.map((coin) => ({
+      id: coin.id,
+      name: coin.name,
+      image: coin.image,
+      current_price: coin.current_price,
+    })));
+    return cryptoCoins;
+  } catch (err) {
+    return err;
+  }
 };
 
 export default fectCrtyptoCoins;
