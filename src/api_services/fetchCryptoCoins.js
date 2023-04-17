710ea5f4e02aca7438fc75a4f4420db78532d475
shubham <shubham.bhoyar077@gmail.com>
2023-04-17 16:25:35 +0530

    Add lofic for fetch crypto coins

---
+++ src/api_services/fetchCryptoCoins.js
@@ -0,0 +1,24 @@
+const fectCrtyptoCoins = async () => {
+  const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?' + new URLSearchParams({
+    vs_currency: 'usd',
+    order: 'market_cap_desc',
+    per_page: 20,
+    page: 1,
+    sparkline: false,
+    locale: 'en'
+  }), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  const cryptoCoins = (data.map((coin) => ({
+    id: coin.id,
+    name: coin.name,
+    image: coin.image,
+    current_price: coin.current_price,
+  })));
+  return cryptoCoins;
+};
+
+export default fectCrtyptoCoins;
