277c9b6c3400afa25107fa2f18b0579fbed328b1
shubham <shubham.bhoyar077@gmail.com>
2023-04-17 17:05:07 +0530

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import IndexLayout from './components/IndexLayout';
+import CryptoList from './components/CrtyptoList';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<IndexLayout />}>
+      <Route index element={<CryptoList />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello There!</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
