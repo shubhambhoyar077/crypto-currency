30a998df8988547cf16ca89fc0a0b4eaa183c5e9
shubham <shubham.bhoyar077@gmail.com>
2023-04-17 20:44:51 +0530

    Fetching coin details for detail page

--- src/api_services/fetchCoinDetails.js
+++ src/api_services/fetchCoinDetails.js
@@ -15,29 +15,37 @@ const fetchCoinDetails = async (coinId) => {
     });
     const data = await response.json();
     const coinDetails = (data.map((coin) => ({
-      market_cap: coin.market_cap,
-      market_cap_rank: coin.market_cap_rank,
-      fully_diluted_valuation: coin.fully_diluted_valuation,
-      total_volume: coin.total_volume,
-      high_24h: coin.high_24h,
-      low_24h: coin.low_24h,
-      price_change_24h: coin.price_change_24h,
-      price_change_percentage_24h: coin.price_change_percentage_24h,
-      market_cap_change_24h: coin.market_cap_change_24h,
-      market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h,
-      circulating_supply: coin.circulating_supply,
-      total_supply: coin.total_supply,
-      max_supply: coin.max_supply,
-      ath: coin.ath,
-      ath_change_percentage: coin.ath_change_percentage,
-      ath_date: coin.ath_date,
-      atl: coin.atl,
-      atl_change_percentage: coin.atl_change_percentage,
-      atl_date: coin.atl_date,
-      last_updated: coin.last_updated,
+      id: coin.id,
+      name: coin.name,
+      image: coin.image,
+      current_price: coin.current_price,
+      details:{
+        market_cap: coin.market_cap,
+        market_cap_rank: coin.market_cap_rank,
+        fully_diluted_valuation: coin.fully_diluted_valuation,
+        total_volume: coin.total_volume,
+        high_24h: coin.high_24h,
+        low_24h: coin.low_24h,
+        price_change_24h: coin.price_change_24h,
+        price_change_percentage_24h: coin.price_change_percentage_24h,
+        market_cap_change_24h: coin.market_cap_change_24h,
+        market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h,
+        circulating_supply: coin.circulating_supply,
+        total_supply: coin.total_supply,
+        max_supply: coin.max_supply,
+        ath: coin.ath,
+        ath_change_percentage: coin.ath_change_percentage,
+        ath_date: coin.ath_date,
+        atl: coin.atl,
+        atl_change_percentage: coin.atl_change_percentage,
+        atl_date: coin.atl_date,
+        last_updated: coin.last_updated,
+      },
     })));
     return coinDetails;
   } catch (err) {
     return err;
   }
 };
+
+export default fetchCoinDetails;
