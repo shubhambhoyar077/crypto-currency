be163334476a48213a63dd480443804dd9e4949a
shubham <shubham.bhoyar077@gmail.com>
2023-04-17 20:23:19 +0530

    Add dynamic link for details page

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import {
 } from 'react-router-dom';
 import IndexLayout from './components/IndexLayout';
 import CryptoList from './components/CryptoList';
+import CryptoCoinDetails from './components/CryptoCoinDetails';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<IndexLayout />}>
       <Route index element={<CryptoList />} />
+      <Route path='/coin/:id' element={<CryptoCoinDetails />} />
     </Route>,
   ),
 );
