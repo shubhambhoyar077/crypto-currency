f67f8196e4d9fff839db86df855f1a04167ccdae
shubham <shubham.bhoyar077@gmail.com>
2023-04-17 17:55:39 +0530

    Create cryptolist components

--- src/components/CrtyptoList.js
+++ src/components/CrtyptoList.js
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCoins } from "../redux/crypto/cryptoSlice";
+
 export default function CryptoList() {
+  const { cryptoCurrency } = useSelector((state) => state.crypto);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch]);
   return (
-    <h1>Hello</h1>
+    <div>
+      <div className="logo-part">
+        <h1>Crypto</h1>
+      </div>
+      <div className="gridContainer">
+
+      </div>
+    </div>
   );
 }
