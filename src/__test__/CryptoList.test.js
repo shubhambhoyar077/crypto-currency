651fa665781b7000da91b12a0dedaec868fbe380
shubham <shubham.bhoyar077@gmail.com>
2023-04-18 21:20:56 +0530

    Add test for render list

---
+++ src/__test__/CryptoList.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import CryptoList from '../components/CryptoList';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+const mockStore = configureMockStore([]);
+
+describe('Crypto component', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      crypto: {
+        cryptoCurrency: [
+          {
+            id: '1',
+            name: 'Coin 1',
+            current_price: 100,
+            image: '#',
+          },
+          {
+            id: '2',
+            name: 'Coin 2',
+            current_price: 100,
+            image: '#',
+          },
+        ],
+      },
+    });
+
+    store.dispatch = jest.fn();
+  });
+
+  it('should render the Coins from store', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+
+          <CryptoList />
+        </Provider>
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByText('Coin 1')).toBeInTheDocument();
+    expect(screen.getByText('Coin 2')).toBeInTheDocument();
+  });
+
+  it('coin count should be 2', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+
+          <CryptoList />
+        </Provider>
+      </BrowserRouter>,
+    );
+
+    expect(screen.getAllByText('$100')).toHaveLength(2);
+  });
+});
