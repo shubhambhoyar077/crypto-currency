736ad1ef21de7b5ac78fbd072b3f73e1756df1ba
shubham <shubham.bhoyar077@gmail.com>
2023-04-18 17:45:42 +0530

    Add circular arrow

--- src/components/CryptoCoin.js
+++ src/components/CryptoCoin.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { BsArrowRightCircle } from 'react-icons/bs';
 
 export default function CryptoCoin({ coin }) {
   return (
     <NavLink to={`/coin/${coin.id}`} className="coin">
+      <BsArrowRightCircle className="details-arrow" />
       <img src={coin.image} alt={coin.name} />
 
       <h3>{coin.name}</h3>
