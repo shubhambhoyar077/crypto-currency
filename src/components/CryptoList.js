c0963a5c680fe5fb339ab7a3c6d244b64da52b58
shubham <shubham.bhoyar077@gmail.com>
2023-04-18 14:13:30 +0530

    Add search bar using name

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCoins } from '../redux/crypto/cryptoSlice';
 import CryptoCoin from './CryptoCoin';
@@ -6,11 +6,23 @@ import CryptoCoin from './CryptoCoin';
 export default function CryptoList() {
   const { cryptoCurrency, isLoading, error } = useSelector((state) => state.crypto);
   const dispatch = useDispatch();
+  const [searchText, setSearchText] = useState('');
+  const [searchList, setSearchList] = useState(cryptoCurrency);
+
   useEffect(() => {
     if (!cryptoCurrency.length) {
       dispatch(fetchCoins());
     }
   }, [dispatch]);
+
+  useEffect(() => {
+    setSearchList(cryptoCurrency);
+  }, [cryptoCurrency]);
+
+  const handelSearch = (e) => {
+    setSearchText(e.target.value);
+    setSearchList(cryptoCurrency.filter((coin) => coin.name.includes(e.target.value)));
+  };
 
   if (isLoading) {
     return (
@@ -21,10 +33,16 @@ export default function CryptoList() {
     return (
       <div>
         <div className="logo-part">
+          <input
+            type="text"
+            value={searchText}
+            onChange={handelSearch}
+            placeholder="Search"
+          />
           <h1>Crypto</h1>
         </div>
         <div className="gridContainer">
-          {cryptoCurrency.map((coin) => <CryptoCoin key={coin.id} coin={coin} />)}
+          {searchList.map((coin) => <CryptoCoin key={coin.id} coin={coin} />)}
         </div>
       </div>
     );
